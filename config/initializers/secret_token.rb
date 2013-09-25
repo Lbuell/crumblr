f498635e40a5c1dfaec9c36fa49bc242b0a9a9f3
Liam Buell <liamcbuell@gmail.com>
2013-09-25 13:13:08 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crumblr::Application.config.secret_key_base = '555f3b2e3cae63e6541fa986d30aed65c6723e183485678f097459d4c19e93e54b923c6a8fee9a1feac2b31308c6b05a07de8d6d1dad612ecce78fff4fb5ab2a'
