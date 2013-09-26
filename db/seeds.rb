14663825f447e7adec0232265f6633005314819f
Liam Buell <liamcbuell@gmail.com>
2013-09-26 10:27:13 -0700

    Added Seeds File

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#Paleo cookies
+post1 = Post.new
+post.title = "Paleo Chocolate Chip Cookies"
+post1.kind = "image"
+post1.content =http://www.parade.com/wp-content/uploads/2013/09/Paleo-Chocolate-Chip-Cookies-1240.jpg
+post1.save
+
+2.times do
+	post1.hearts.create	
+end
+
+#Ovenly
+post2 = Post.new(title: "Ovenly", kind: "text")
+post2.content = "tals Fav bakery"
+post2.save
+
+5.times {post2.hearts.create}
+
+#Paleo Brownies
+post3 = Post.create (
+	title: "Paleo Brownies", 
+	kind: "image", 
+	content: "http://livinghealthywithchocolate.com/wp-content/uploads/2013/06/Paleo-Gluten-Free-Brownie.jpg")
+
+post3.save
+
+8.times {post3.hearts.create}
