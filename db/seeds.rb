f2e0ff41b868440e8c75d326fdcab29e8ae440da
Liam Buell <liamcbuell@gmail.com>
2013-09-26 11:24:25 -0700

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 #Paleo cookies
 post1 = Post.new
-post.title = "Paleo Chocolate Chip Cookies"
+post1.title = "Paleo Chocolate Chip Cookies"
 post1.kind = "image"
-post1.content =http://www.parade.com/wp-content/uploads/2013/09/Paleo-Chocolate-Chip-Cookies-1240.jpg
+post1.content = http://www.parade.com/wp-content/uploads/2013/09/Paleo-Chocolate-Chip-Cookies-1240.jpg
 post1.save
 
 2.times do
@@ -21,14 +22,9 @@ post2 = Post.new(title: "Ovenly", kind: "text")
 post2.content = "tals Fav bakery"
 post2.save
 
-5.times {post2.hearts.create}
+5.times { post2.hearts.create }
 
 #Paleo Brownies
-post3 = Post.create (
-	title: "Paleo Brownies", 
-	kind: "image", 
-	content: "http://livinghealthywithchocolate.com/wp-content/uploads/2013/06/Paleo-Gluten-Free-Brownie.jpg")
-
+post3 = Post.create(title: "Paleo Brownies", kind: "image",	content: "http://livinghealthywithchocolate.com/wp-content/uploads/2013/06/Paleo-Gluten-Free-Brownie.jpg")
 post3.save
-
-8.times {post3.hearts.create}
+8.times { post3.hearts.create }
